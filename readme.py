81611d277264d759712af4c9c52e472c60bf6a19
Sangwonseo <43750294+sangwonseo94@users.noreply.github.com>
2021-09-04 09:32:02 +0900

    Update readme.py

--- readme.py
+++ readme.py
@@ -12,14 +12,14 @@ for idx, feed in enumerate(rss_feed['entries']):
     latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
 
 
-markdown_text = """### 
-⚡ Email (Company) : sangwonseo@woorifis.com
-⚡ Email (Private) : gotkddnjs@naver.com
-⚡ Software Engineer in WooriFis ( Woori Finance Information System : Period 2020.12 ~ Now )
-⚡ Blog : https://algorithmstudy-mju.tistory.com/
-⚡ [![github stats]  (https://github-readme-stats.vercel.app/api/top-langs/?username=sangwonseo94&layout=compact)](https://github.com/anuraghazra/github-readme-stats)](https://algorithmstudy-mju.tistory.com/)
-⚡ [![Solved.ac프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=gotkddnjs)](https://solved.ac/gotkddnjs)<!--[![CodeForces Profile](https://cf.leed.at?id=sangwon)](https://codeforces.com/profile/sangwon)  -->
-⚡ My Blog Post """
+markdown_text = """  ### 
+⚡ Email (Company) : sangwonseo@woorifis.com  
+⚡ Email (Private) : gotkddnjs@naver.com  
+⚡ Software Engineer in WooriFis ( Woori Finance Information System : Period 2020.12 ~ Now )  
+⚡ Blog : https://algorithmstudy-mju.tistory.com/  
+⚡ [![github stats]  (https://github-readme-stats.vercel.app/api/top-langs/?username=sangwonseo94&layout=compact)](https://github.com/anuraghazra/github-readme-stats)](https://algorithmstudy-mju.tistory.com/)  
+⚡ [![Solved.ac프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=gotkddnjs)](https://solved.ac/gotkddnjs)<!--[![CodeForces Profile](https://cf.leed.at?id=sangwon)](https://codeforces.com/profile/sangwon)   
+⚡ My Blog Post   """
 readme_text = f"{markdown_text}{latest_blog_post_list}"
 with open("README.md", 'w', encoding='utf-8') as f: 
     f.write(readme_text)
